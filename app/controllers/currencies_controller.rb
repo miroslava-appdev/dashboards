4de01811e39374cbc6dca7d50f5b2f1d2c89a878
Miroslava Osorio <miroslavaosorio1@gmail.com>
2023-04-18 17:23:25 +0000

    forex route completed

---
+++ app/controllers/currencies_controller.rb
@@ -0,0 +1,15 @@
+class CurrenciesController < ApplicationController
+   def first_currency
+    # in rails it comes with the ability to open read the string but other types of code need: require "open-uri"
+    
+    @raw_data = open("https://api.exchangerate.host/symbols").read
+    #the following parse method helps read the data in a nice and clean way, rather than long lines of non-stop data
+    @parsed_data = JSON.parse(@raw_data)
+    #this adds the hash rockets and cleans it up a bit but there are still long lines of code
+    @symbols_hash = @parsed_data.fetch("symbols")
+    #this helped clean up the format of our data by eliminating things and keeping the keys such as AED, AFN, etc. Now, we need to work with the keys
+    @array_of_symbols = @symbols_hash.keys
+    render({:template => "currency_templates/step_one.html.erb"})
+
+  end
+end
